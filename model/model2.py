254611cafdd5348aebaffc363aa28e3a229ed502
Harry Mellsop <harrymellsop@gmail.com>
2019-09-26 22:07:03 -0700

    Test

--- model/model2.py
+++ model/model2.py
@@ -5,5 +5,11 @@ class HammeredModel:
     
     def train(dataset, learningRate = 0.1, numIterations = 1000, batchSize = 100, displayStep = 2):
         print("Gooday mate")
+        return
+
+    # returns true if the audio sample is drunk, returns false if the sample is sober
+    def inference(audioSample) -> Bool:
+        print("He's drunk as a skunk mate")
+        return true
 
     
