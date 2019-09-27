5eacf827d1cdd49c1dc611d23a3a5e948e6d0acc
Harry Mellsop <harrymellsop@gmail.com>
2019-09-26 22:13:56 -0700

    Fleshing that model out

--- model/model2.py
+++ model/model2.py
@@ -2,14 +2,25 @@ import tensorflow as tf
 import numpy as np
 
 class HammeredModel:
-    
-    self def train(dataset, learningRate = 0.1, numIterations = 1000, batchSize = 100, displayStep = 2):
+
+    def __init__(self, trainingDataset, testingDataset, learningRate, numIterations):
+        self.trainingDataset = trainingDataset
+        self.testingDataset = testingDataset
+        self.learningRate = learningRate
+        self.numIterations = numIterations
+
+    def train():
         print("Gooday mate")
         return
 
     # returns true if the audio sample is drunk, returns false if the sample is sober
-    self def inference(audioSample) -> Boolean:
+    def inference(audioSample) -> bool:
         print("He's drunk as a skunk mate")
         return True
 
-    
+
+fred = HammeredModel([1, 2], [3, 4], 0.01, 1000)
+print(fred.trainingDataset)
+print(fred.testingDataset)
+print(fred.learningRate)
+print(fred.numIterations)
