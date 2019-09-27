09e7c2408bd91897051d154a744bfdc46416d83e
Harry Mellsop <harrymellsop@gmail.com>
2019-09-26 22:43:44 -0700

    Model break-apart

--- model/HammeredModel.py
+++ model/HammeredModel.py
@@ -29,24 +29,3 @@ class HammeredModel:
     def inference(self, processedAudioSample) -> bool:
         return self.model.predict(processedAudioSample)
 
-
-# AudioProcessor class provides standarised functionality to perform conversions between .wav files 
-# and interpretable tensors for use in the HammeredModel.  See method comments for guidance on how to use
-class AudioProcessor:
-
-    def __init__(self):
-        print("Audio processor at your service.")
-
-    # perform some processing to the audio sample, such that we can feed it to the model
-    def processAudioSample(self, audioSample):
-        
-        return audioSample
-
-    # takes a raw array of .wav file and boolean pairs, then breaks them down into tensors for training
-    # and testing sets.  Returns them all for use in initialising the model
-    def prepareData(self, rawData):
-        trainX = "hello"
-        trainY = "goodbye"
-        testX = "yellow"
-        testY = "mellow"
-        return trainX, trainY, testX, testY
