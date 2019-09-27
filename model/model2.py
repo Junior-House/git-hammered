2be9ca8e09da618441d8388af60d7dfa9589ea91
Harry Mellsop <harrymellsop@gmail.com>
2019-09-26 22:08:20 -0700

    Fixed typo

--- model/model2.py
+++ model/model2.py
@@ -3,13 +3,13 @@ import numpy as np
 
 class HammeredModel:
     
-    def train(dataset, learningRate = 0.1, numIterations = 1000, batchSize = 100, displayStep = 2):
+    self def train(dataset, learningRate = 0.1, numIterations = 1000, batchSize = 100, displayStep = 2):
         print("Gooday mate")
         return
 
     # returns true if the audio sample is drunk, returns false if the sample is sober
-    def inference(audioSample) -> Bool:
+    self def inference(audioSample) -> Boolean:
         print("He's drunk as a skunk mate")
-        return true
+        return True
 
     
